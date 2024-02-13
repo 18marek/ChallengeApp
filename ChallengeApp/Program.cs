960a2d3a51ad77007181a2a82fb0eeb0b9f26330
Marek kozubski <rentier1@op.pl>
2024-02-13 11:43:59 +0100

    Zadanie 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "man";
+int age = 15;
+
+if(sex == "women")
+    {
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (name == "Ewa" && age == 30)
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+
+}
+else if (sex == "man" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+
+Console.ReadKey();
+
+
+
+
+
